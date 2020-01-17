da788b8865be6638fdeb0ec2a2941c5627f7b065
satishderle123 <satish.derle@gmail.com>
2020-01-17 22:47:57 +0530

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,15 @@
 // app.component.ts
 
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent implements OnInit {
-   constructor() { }
-   ngOnInit() {
-   }
-   onClickSubmit(formData) {
-      alert('Your Email is : ' + formData.email);
-   }
+  constructor() {}
+  ngOnInit() {}
+  onClickSubmit(formData) {
+    alert("Your Email is : " + formData.email);
+  }
 }
